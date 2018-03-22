b18ead4c4b3dfc1c09e9a37913d4293384d8924c
UladKasach <nappa300@gmail.com>
2018-03-22 13:19:44 -0400

    add convinience property for requesting the injected require type be synchronous / asynchronous

--- src/utilities/normalize_request_options.js
+++ src/utilities/normalize_request_options.js
@@ -4,6 +4,7 @@ module.exports = function(options){
         var current_path = window.location.href;
         options.relative_path_root = current_path.substring(0, current_path.lastIndexOf("/")) + "/";
     };
+    if(typeof options.require !== "undefined") options.injection_require_type = options.require; // convinience casting
     if(typeof options.injection_require_type == "undefined"){ // if injection require type is not defined, default to async
         options.injection_require_type = "async";
     }
